0116136d8d5df0bdafdc6b85b227c976f4718669
yaejin12 <pos03167@naver.com>
2024-08-03 12:13:11 +0900

    ✅ main2병합

--- src/main/java/com/project/api/metting/controller/MainController.java
+++ src/main/java/com/project/api/metting/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.project.api.metting.controller;
 
+import com.project.api.metting.entity.Group;
+import com.project.api.metting.service.MainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -7,17 +9,24 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @CrossOrigin
 public class MainController {
 
+    private final MainService mainService;
+
 
 //    미팅 리스트 전체 조회
-    @GetMapping("/")
-    public ResponseEntity<?> getMeetingList(){
-        return ResponseEntity.ok().body("");
+    @GetMapping("/main")
+    public ResponseEntity<?> getMeetingList() {
+
+        List<Group> meetingList = mainService.getMeetingList();
+
+        return ResponseEntity.ok().body(meetingList);
     }
 
 
