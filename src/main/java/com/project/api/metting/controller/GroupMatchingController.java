ca21895cfa052e1418eec8f9cd959844b09dbc01
munjieun <mun.mimi925@gmail.com>
2024-08-06 11:35:21 +0900

    :white_check_mark: GroupMatchingService 예외 처리

--- src/main/java/com/project/api/metting/controller/GroupMatchingController.java
+++ src/main/java/com/project/api/metting/controller/GroupMatchingController.java
@@ -6,6 +6,7 @@ import com.project.api.metting.entity.Group;
 import com.project.api.metting.service.GroupMatchingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,18 +22,20 @@ public class GroupMatchingController {
 
     /**
      * 매칭 요청(신청자 역할)
-     * @param requestDto
+     * @param groupMatchingRequestDto - 히스토리 정보에 추가될 리퀘스트 정보 dto
+     * @return - 요청 처리 body
      */
-    @PostMapping("/request")
-    public void matchingRequest(GroupMatchingRequestDto requestDto) {
+    @PostMapping("/createRequest")
+    public ResponseEntity<String> createRequest(@RequestBody GroupMatchingRequestDto groupMatchingRequestDto) {
+        // 1. 히스토리 생성 요청
+        groupMatchingService.createHistory(groupMatchingRequestDto);
 
-        // 히스토리 생성 요청
-        groupMatchingService.createHistory(requestDto);
 
+        return ResponseEntity.status(HttpStatus.CREATED).body("매칭 신청이 성공적으로 생성되었습니다.");
     }
 
     /**
-     * 매칭 요청 열람(주최자 기준)
+     * 매칭 요청 리스트 열람(주최자 기준)
      * @param groupId - 주최자 그룹 아이디
      * @return - 신청자 리스트 반환
      */
