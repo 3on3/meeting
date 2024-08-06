1c9c2da3d4fb9d2b87233ebc2ad9e0256b3fd2db
JinWooP98 <qwdk0406@naver.com>
2024-08-06 11:39:12 +0900

    :fire: 채팅 저장 기능

--- src/main/java/com/project/api/metting/service/ChatMessageService.java
+++ src/main/java/com/project/api/metting/service/ChatMessageService.java
@@ -1,9 +1,13 @@
 package com.project.api.metting.service;
 
+import com.project.api.auth.TokenProvider;
+import com.project.api.metting.dto.response.ChatMessageResponseDto;
 import com.project.api.metting.entity.ChatMessage;
 import com.project.api.metting.entity.ChatRoom;
+import com.project.api.metting.entity.User;
 import com.project.api.metting.repository.ChatMessagesRepository;
 import com.project.api.metting.repository.ChatRoomsRepository;
+import com.project.api.metting.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +24,7 @@ public class ChatMessageService {
     private static final Logger log = LoggerFactory.getLogger(ChatMessageService.class);
     private final ChatMessagesRepository chatMessagesRepository;
     private final ChatRoomsRepository chatRoomsRepository;
+    private final UserRepository userRepository;
 
     public List<ChatMessage> finAllMessage(String chatRoomId) {
 
@@ -31,5 +36,20 @@ public class ChatMessageService {
         System.out.println("chatMessage = " + chatMessage);
 
         return chatMessage;
+    }
+
+    public ChatMessage saveChatMessage(ChatMessageResponseDto chatMessageResponseDto, String userId) {
+
+        User user = userRepository.findById(userId).orElse(null);
+        ChatRoom chatRoom = chatRoomsRepository.findById(chatMessageResponseDto.getRoomId()).orElse(null);
+
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .user(user)
+                .messageContent(chatMessageResponseDto.getMessage())
+                .build();
+
+        ChatMessage savedMessage = chatMessagesRepository.save(chatMessage);
+        return savedMessage;
     }
 }
