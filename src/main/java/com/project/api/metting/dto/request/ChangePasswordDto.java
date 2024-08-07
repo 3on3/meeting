2ab62e713a0ae1d128efb7858dd65ccd77e944df
kimeunji <eunji93121@gmail.com>
2024-08-07 16:15:53 +0900

    feat : mypage password change

--- src/main/java/com/project/api/metting/dto/request/ChangePasswordDto.java
+++ src/main/java/com/project/api/metting/dto/request/ChangePasswordDto.java
@@ -1,12 +1,15 @@
 package com.project.api.metting.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ChangePasswordDto {
 
     private String currentPassword; //현재 비밀번호
     private String newPassword; //새로운 비밀번호
     private String confirmNewPassword; //새로운 비밀번호 확인
+
 
 }
