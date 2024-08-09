b8320a1ca94ba0499dcf1471e589a88d4f7ce55d
JinWooP98 <qwdk0406@naver.com>
2024-08-09 10:28:48 +0900

    :fire: 웹소켓 합치기

--- src/main/java/com/project/api/handler/ChatWebSocketHandler.java
+++ src/main/java/com/project/api/handler/ChatWebSocketHandler.java
@@ -1,12 +1,12 @@
-package com.project.api.testChat;
+
+package com.project.api.handler;
+
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.project.api.metting.dto.request.ChatMessageRequestDto;
-import com.project.api.metting.dto.response.LoginResponseDto;
-import com.project.api.metting.dto.response.MainWebSocketResponseDto;
-import com.project.api.metting.entity.User;
+import com.project.api.metting.dto.response.ChatWebSocketResponseDto;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
@@ -19,15 +19,15 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class MainWebSocketHandler extends TextWebSocketHandler {
+
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
 
     private final ObjectMapper objectMapper;
     private final Map<String, WebSocketSession> sessions;
-    private final Map<String, LoginResponseDto> users = new ConcurrentHashMap<>();
 
-    public MainWebSocketHandler(Map<String, WebSocketSession> sessions) {
+    public ChatWebSocketHandler(Map<String, WebSocketSession> sessions) {
         this.objectMapper = new ObjectMapper();
         // localDateTime 을 제대로 받아오기 위한 코드
         this.objectMapper.registerModule(new JavaTimeModule());
@@ -50,26 +50,24 @@ public class MainWebSocketHandler extends TextWebSocketHandler {
     //양방향 데이터 통신할 떄 해당 메서드가 call 된다.
     @OnMessage
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        //do something
 
-        String sessionId = session.getId();
+        System.out.println("session = " + message.getPayload());
 
-        MainWebSocketResponseDto mainWebSocketResponseDto = objectMapper.readValue(message.getPayload(), MainWebSocketResponseDto.class);
+        ChatWebSocketResponseDto data = objectMapper.readValue(message.getPayload(), ChatWebSocketResponseDto.class);
 
+        System.out.println("data = " + data);
 
-        if(mainWebSocketResponseDto.getType().equals("login")) {
-            users.put(sessionId, mainWebSocketResponseDto.getLoginUser());
+        if(data.getType().equals("message")) {
+            sessions.values().forEach((s) -> {
+                try {
+                    String jsonMessage = objectMapper.writeValueAsString(data.getMessage());
+                    s.sendMessage(new TextMessage(jsonMessage));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
-
-        System.out.println("users = " + users);
-
-        sessions.values().forEach((s) -> {
-            try {
-                String jsonMessage = objectMapper.writeValueAsString(null);
-                s.sendMessage(new TextMessage(jsonMessage));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
     }
 
     //웹소켓 종료
@@ -77,7 +75,6 @@ public class MainWebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         final String sessionId = session.getId();
         try {
-            users.remove(sessionId);
             sessions.remove(sessionId); // 삭제
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -86,10 +83,7 @@ public class MainWebSocketHandler extends TextWebSocketHandler {
     //통신 에러 발생 시
     @OnError
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-
         System.out.println("session = " + session);
-
-
     }
 
     private void sendMessage(String sessionId, WebSocketMessage<?> message) {
@@ -101,4 +95,7 @@ public class MainWebSocketHandler extends TextWebSocketHandler {
             }
         });
     }
+
+
+
 }
