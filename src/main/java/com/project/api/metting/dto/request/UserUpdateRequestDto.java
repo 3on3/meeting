95470216a52c2484ba032e1ee6438ced6c04dc4a
kimeunji <eunji93121@gmail.com>
2024-08-12 01:16:38 +0900

    feat : removeUser

---
+++ src/main/java/com/project/api/metting/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,31 @@
+package com.project.api.metting.dto.request;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class UserUpdateRequestDto {
+
+    //    @JsonProperty("product_img")
+    private String profileImg; // 프로필 이미지 경로
+
+    //    @JsonProperty("profile_introduce")
+    private String profileIntroduce; // 프로필 소개
+
+    private String nickname; // 닉네임
+
+    private String membership; // 멤버십 등급
+
+    private String univ; // 대학
+
+    private String major; // 전공
+
+
+}
