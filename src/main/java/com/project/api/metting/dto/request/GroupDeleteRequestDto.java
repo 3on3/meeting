830ecef70d59d4a228a07861b45e35e313b2c88d
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-12 10:19:25 +0900

    :white_check_mark: 그룹 삭제 버튼 추가

---
+++ src/main/java/com/project/api/metting/dto/request/GroupDeleteRequestDto.java
@@ -0,0 +1,14 @@
+package com.project.api.metting.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDeleteRequestDto {
+    private String groupId;
+}
