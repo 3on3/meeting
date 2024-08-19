86e27fa90daeec19058481aa1fcc4cf48e47d37d
yaejin12 <pos03167@naver.com>
2024-08-19 11:52:50 +0900

    :white_check_mark: membershipAuth 정보 DTO에 매핑 코드 정리

--- src/main/java/com/project/api/metting/repository/UserMembershipRepository.java
+++ src/main/java/com/project/api/metting/repository/UserMembershipRepository.java
@@ -3,5 +3,5 @@ package com.project.api.metting.repository;
 import com.project.api.metting.entity.UserMembership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserMembershipRepository extends JpaRepository<UserMembership, String>,UserMembershipRepositoryCustom {
+public interface UserMembershipRepository extends JpaRepository<UserMembership, String> {
 }
--- src/main/java/com/project/api/metting/repository/UserMembershipRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.project.api.metting.repository;
-
-import com.project.api.metting.entity.Membership;
-import com.project.api.metting.entity.User;
-
-public interface UserMembershipRepositoryCustom {
-
-//    Membership membershipAuthFind(String userEmail);
-}
