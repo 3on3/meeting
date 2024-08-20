59bcd5a976dd71b30479aa4cc788ab61e7451868
munjieun <mun.mimi925@gmail.com>
2024-08-20 15:13:20 +0900

    :white_check_mark: 보드 , 댓글 레포지토리 파일 생성

---
+++ src/main/java/com/project/api/metting/repository/BoardReplyRepository.java
@@ -0,0 +1,7 @@
+package com.project.api.metting.repository;
+
+import com.project.api.metting.entity.BoardReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardReplyRepository extends JpaRepository<BoardReply, String> {
+}
