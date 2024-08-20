0ebdfaf586c3028c19095752063b140872e3f43d
munjieun <mun.mimi925@gmail.com>
2024-08-20 11:24:53 +0900

    :tada: Board, BoardReply 엔터티 생성

---
+++ src/main/java/com/project/api/metting/entity/Board.java
@@ -0,0 +1,51 @@
+package com.project.api.metting.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+/**
+ * Board
+ * : 게시글 정보를 담은 엔터티
+ * author
+ * : 해당 게시글의 작성자(M : 1 - User)
+ */
+@Getter
+@ToString()
+@EqualsAndHashCode(of ="id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "mt_boards")
+public class Board {
+
+    @Id
+    @GenericGenerator(strategy = "uuid2", name = "uuid-generator")
+    @GeneratedValue(generator = "uuid-generator")
+    @Column(name = "mt_board_id")
+    private String id; // PK
+
+    @Column(name = "mt_board_title", nullable = false)
+    private String title; // 제목
+
+    @Column(name = "mt_board_content", nullable = false)
+    private String content; // 내용
+
+    @Column(name = "mt_board_created_at")
+    @Builder.Default
+    private LocalDateTime created_at = LocalDateTime.now(); // 작성 일자
+
+    @Column(name = "mt_board_is_deleted", nullable = false)
+    @Builder.Default
+    private Boolean isDeleted = false; // 삭제 여부
+
+    @Column(name = "mt_board_view_count", nullable = false)
+    @Builder.Default
+    private Integer viewCount = 0; // 조회수
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mt_user_id", nullable = false)
+    private User author; // 작성자
+}
---
+++ src/main/java/com/project/api/metting/entity/BoardReply.java
@@ -0,0 +1,50 @@
+package com.project.api.metting.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * BoardReply
+ * : 게시글에 대한 댓글 정보를 담은 엔터티
+ * author, boardId
+ * : 해당 댓글의 작성자, 보드 아이디 (M : 1 - User, Board)
+ */
+@Getter
+@ToString()
+@EqualsAndHashCode(of ="id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "mt_board_repies")
+public class BoardReply {
+
+    @Id
+    @GenericGenerator(strategy = "uuid2", name = "uuid-generator")
+    @GeneratedValue(generator = "uuid-generator")
+    @Column(name = "mt_board_reply_id")
+    private String id; // PK
+
+    @Column(name = "mt_board_reply_content" , nullable = false)
+    private String content; // 내용
+
+    @Column(name = "mt_board_reply_created_at" , nullable = false)
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now(); // 작성 일자
+
+    @Column(name = "mt_board_reply_is_deleted" , nullable = false)
+    @Builder.Default
+    private Boolean isDeleted = false; // 삭제 여부
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mt_user_id")
+    private User author; // 작성자
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mt_board_id" , nullable = false)
+    private Board board; // 보드
+
+}
