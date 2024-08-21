8f41442cd73f37cfcfaf2e94b2787e6ca7b33b31
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-21 14:28:38 +0900

    :white_check_mark: 주석처리

--- src/main/java/com/project/api/metting/controller/GroupController.java
+++ src/main/java/com/project/api/metting/controller/GroupController.java
@@ -24,6 +24,13 @@ import static com.project.api.auth.TokenProvider.TokenUserInfo;
 public class GroupController {
     private final GroupService groupService;
 
+
+    /**
+     *
+     * @param dto - 그룹 생성에 필요한 dto
+     * @param tokenInfo - 로그인한 유저의 token
+     * @return - 성공하면 text 전송
+     */
     @PostMapping("/create")
     public ResponseEntity<?> GroupCreate(@RequestBody GroupCreateDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         try {
@@ -36,19 +43,13 @@ public class GroupController {
         }
     }
 
-    @PostMapping("/join")
-    public ResponseEntity<?> GroupJoin(@RequestBody GroupJoinRequestDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
-        try {
-            groupService.joinGroup(dto, tokenInfo);
-            return ResponseEntity.ok().body("가입 신청이 완료되었습니다.");
-        } catch (IllegalStateException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("그룹 참여 신청에 실패하였습니다. 다시 시도해주세요.");
-        }
-    }
 
-
+    /**
+     *
+     * @param dto - 그룹 삭제에 필요한 dto
+     * @param tokenInfo - 로그인한 유저의 token 정보
+     * @return - 성공하면 그룹 삭제 성공 텍스트 전송
+     */
     @PostMapping("/delete")
     public ResponseEntity<?> GroupDelete(@RequestBody GroupDeleteRequestDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         try {
@@ -61,12 +62,26 @@ public class GroupController {
         }
     }
 
+
+    /**
+     *
+     * @param groupId - 그룹에 참여신청한 유저의 목록
+     * @param tokenInfo - 로그인한 유저의 token 정보
+     * @return - 성공한 유저 목록
+     */
     @GetMapping("/invite/{groupId}")
     public ResponseEntity<?> getGroup(@PathVariable String groupId, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         List<InviteUsersViewResponseDto> joinRequests = groupService.getJoinRequests(groupId, tokenInfo);
         return ResponseEntity.ok().body(joinRequests);
     }
 
+
+    /**
+     *
+     * @param groupUserId - 그룹에 참여신청한 유저 수락하는 메서드
+     * @param tokenInfo - 로그인한 유저의 토큰 정보
+     * @return - 성공하면 텍스트 리턴
+     */
     @PostMapping("/join-requests/{groupUserId}/accept")
     public ResponseEntity<String> acceptJoinRequest(@PathVariable String groupUserId, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
 
@@ -81,6 +96,12 @@ public class GroupController {
     }
 
 
+    /**
+     *
+     * @param dto - 그룹 탈퇴에 필요한 dto 정보
+     * @param tokenInfo - 로그인 한 유저의 토큰 정보
+     * @return - 성공 ok 전송
+     */
     @PostMapping("/withdraw")
     public ResponseEntity<Void> groupWithDraw(@RequestBody GroupWithdrawRequestDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         groupService.groupWithDraw(dto, tokenInfo);
@@ -88,6 +109,12 @@ public class GroupController {
     }
 
 
+    /**
+     *
+     * @param groupUserId - 가입신청을 거절한 유저의 groupuserId
+     * @param tokenInfo - 로그인한 유저의 토큰 정보
+     * @return - 거절 텍스트 리턴
+     */
     @PostMapping("/join-requests/{groupUserId}/cancel")
     public ResponseEntity<?> rejectJoinRequest(@PathVariable String groupUserId, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         try {
@@ -100,6 +127,13 @@ public class GroupController {
         }
     }
 
+
+    /**
+     *
+     * @param dto - 그룹에서 추방할 정보의 dto
+     * @param tokenInfo - 로그인 한 유저의 dto
+     * @return - 성공하면 text 전송
+     */
     @PostMapping("/exile")
     public ResponseEntity<?> exileUser(@RequestBody GroupExileDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         log.info("dto info 123 - {}", dto.getGroupId());
@@ -113,6 +147,13 @@ public class GroupController {
         }
     }
 
+
+    /**
+     *
+     * @param code - 해당 그룹에 가입할 수 있는 초대코드
+     * @param tokenInfo - 로그인 한 유저의 토큰 정보
+     * @return - 성공하면 그룹이름을 리턴
+     */
     @PostMapping("/join/invite")
     public ResponseEntity<?> joinGroupWithInviteCode(@RequestParam String code, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         if (code == null || code.isEmpty()) {
@@ -143,6 +184,12 @@ public class GroupController {
     }
 
 
+    /**
+     * 초대코드를 생성하는 메서드
+     * @param dto - 초대코드 생성에 필요한 groupId dto,
+     * @param tokenInfo - 로그인한 유저의 정보
+     * @return - 초대링크랑 남은시간 리턴
+     */
     @PostMapping("/inviteCodeGenerate")
     public ResponseEntity<?> inviteCodeSelect(@RequestBody GroupDeleteRequestDto dto, @AuthenticationPrincipal TokenUserInfo tokenInfo) {
         InviteCodeResponseDto inviteCodeResponseDto = groupService.generateGroupInviteCode(dto.getGroupId());
