3a0e65bc3c7d14b8f279ff2963c2755468f56414
munjieun <mun.mimi925@gmail.com>
2024-08-21 14:33:18 +0900

    :memo: board엔터티 수정

--- src/main/java/com/project/api/metting/entity/Board.java
+++ src/main/java/com/project/api/metting/entity/Board.java
@@ -1,5 +1,6 @@
 package com.project.api.metting.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -27,15 +28,23 @@ public class Board {
     @Column(name = "mt_board_id")
     private String id; // PK
 
+    @Setter
     @Column(name = "mt_board_title", nullable = false)
     private String title; // 제목
 
+    @Setter
     @Column(name = "mt_board_content", nullable = false)
     private String content; // 내용
+
+
 
     @Column(name = "mt_board_created_at")
     @Builder.Default
     private LocalDateTime createdAt = LocalDateTime.now(); // 작성 일자
+
+    @Setter
+    @Column(name = "mt_modified_at")
+    private LocalDateTime modifiedAt;
 
     @Column(name = "mt_board_is_deleted", nullable = false)
     @Builder.Default
@@ -45,6 +54,10 @@ public class Board {
     @Builder.Default
     private Integer viewCount = 0; // 조회수
 
+
+
+    @JsonIgnore
+    @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mt_user_id", nullable = false)
     private User author; // 작성자
