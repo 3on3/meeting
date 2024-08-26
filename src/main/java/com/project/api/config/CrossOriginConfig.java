da79c1bd7391eb1204a6d8b252226ff1ee326e49
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-26 12:03:12 +0900

    :white_check_mark: https added

--- src/main/java/com/project/api/config/CrossOriginConfig.java
+++ src/main/java/com/project/api/config/CrossOriginConfig.java
@@ -14,7 +14,7 @@ public class CrossOriginConfig implements WebMvcConfigurer {
             "http://localhost:3000",
             "http://localhost:3001",
             "http://localhost:3002",
-            "http://gwating.com.s3-website.ap-northeast-2.amazonaws.com"
+            "https://gwating.com.s3-website.ap-northeast-2.amazonaws.com"
     };
     @Override
     public void addCorsMappings(CorsRegistry registry) {
