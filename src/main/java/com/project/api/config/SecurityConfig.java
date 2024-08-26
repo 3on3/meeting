f4b8173dc4559eb701c5cdf72ff60d097edc71b5
hun2zz <tkdgnsdldkdlel@naver.com>
2024-08-26 12:59:35 +0900

    :white_check_mark: 시큐리티 콘피그 수정

--- src/main/java/com/project/api/config/SecurityConfig.java
+++ src/main/java/com/project/api/config/SecurityConfig.java
@@ -15,52 +15,35 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Arrays;
 
-// 권한처리
-// OAuth2 - SNS로그인
 @EnableWebSecurity
 @RequiredArgsConstructor
-//컨트롤러에서 사전, 사후에 권한정보를 캐치해서 막을건지.
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
-    // 비밀번호 암호화 객체 컨테이너에 등록 (스프링에게 주입받는 설정)
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
                 .cors()
                 .and()
-                .csrf().disable() // 필터설정 off
-                .httpBasic().disable() // 베이직 인증 off
-                .formLogin().disable() // 로그인창 off
-                //세션 인증은 더 이상 사용하지 않음
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests() //요청 별로 인가 설정
-                //아래의 요청은 모두 허용
-
+                .authorizeRequests()
                 .antMatchers("/signup/**","/login", "/intro", "/socket/**", "/password/**", "/board/**").permitAll()
+                .anyRequest().authenticated();
 
-                //나머지 인증은 전부 인증(로그인) 후 진행해라.
-                .anyRequest().authenticated() //인가 설정 on
-        ;
-
-        //토큰 위조 검사 커스텀 필터 필터체인에 연결
-        //CorsFilteR(spring의 필터) 뒤에 커스텀 필터를 연결
         http.addFilterAfter(jwtAuthFilter, CorsFilter.class);
 
         return http.build();
@@ -69,12 +52,20 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002",
+                "https://gwating.com",
+                "https://www.gwating.com",
+                "https://d1jw85cmg05158.cloudfront.net",
+                "https://3.38.26.248"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // allowCredentials 설정 추가
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
